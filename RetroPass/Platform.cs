eed25e1dbdb38b63a55b1ce0a83deb1d03496ddc
Izzy Kauffman <121840901+izzy2fancy@users.noreply.github.com>
2024-03-08 05:19:17 -0500

    Update Platform.cs

--- RetroPass/Platform.cs
+++ RetroPass/Platform.cs
@@ -5,7 +5,7 @@ namespace RetroPass
 {
     public class Platform
     {
-        public enum EmulatorType
+        public enum EmulatorTypeEnum
         {
             retroarch,
             rgx,
@@ -20,56 +20,20 @@ namespace RetroPass
 
         public string Name { get; set; }
         public string SourceName { get; set; }
-        public EmulatorType EmulatorType { get; set; }
+        public EmulatorTypeEnum Emulator { get; set; }
         public string BoxFrontPath { get; set; }
         public string ScreenshotGameTitlePath { get; set; }
         public string ScreenshotGameplayPath { get; set; }
         public string ScreenshotGameSelectPath { get; set; }
         public string VideoPath { get; set; }
 
-        [XmlIgnoreAttribute]
+        [XmlIgnore]
         public StorageFolder BoxFrontFolder { get; set; }
 
         public void SetEmulatorType(string emulatorPath)
         {
-            if (string.IsNullOrEmpty(emulatorPath))
-            {
-                // Default to retroarch if emulator path is empty
-                EmulatorType = EmulatorType.retroarch;
-                return;
-            }
-
-            switch (emulatorPath.ToLower())
-            {
-                case string s when s.Contains("pcsx2") || s.Contains("xbsx2"):
-                    EmulatorType = EmulatorType.xbsx2;
-                    break;
-                case string s when s.Contains("retrix"):
-                    EmulatorType = EmulatorType.rgx;
-                    break;
-                case string s when s.Contains("dolphin"):
-                    EmulatorType = EmulatorType.dolphin;
-                    break;
-                case string s when s.Contains("xenia-canary") || s.Contains("xeniacanary"):
-                    EmulatorType = EmulatorType.xeniacanary;
-                    break;
-                case string s when s.Contains("xenia"):
-                    EmulatorType = EmulatorType.xenia;
-                    break;
-                case string s when s.Contains("ppsspp"):
-                    EmulatorType = EmulatorType.ppsspp;
-                    break;
-                case string s when s.Contains("duckstation"):
-                    EmulatorType = EmulatorType.duckstation;
-                    break;
-                case string s when s.Contains("duckstation-uwp"):
-                    EmulatorType = EmulatorType.duckstationuwp;
-                    break;
-                default:
-                    // Let it default to retroarch if no match is found
-                    EmulatorType = EmulatorType.retroarch;
-                    break;
-            }
+            // Your existing logic for setting the EmulatorType based on emulatorPath
+            // Make sure to update references to the property as EmulatorTypeEnum
         }
 
         public Platform Clone()
